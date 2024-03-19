6689d7142e2ccf583d268fdd38c1bfe7cb661862
DJ5007 <kupchak78@gmail.com>
2024-03-19 01:41:01 +0100

    Add error message in DataFetcing

--- src/DataFetching/DataFetching.jsx
+++ src/DataFetching/DataFetching.jsx
@@ -6,48 +6,55 @@ import Prelaoder from "../Preloader/Preloader.jsx";
 
 function DataFetching() {
 	const [isLoading, setIsloading] = useState(false);
+	const [error, setError] = useState();
 	const [data, setData] = useState([]);
 	// const [marketCR, setMarketCR] = useState("");
 
 	useEffect(() => {
 		const fetchCrupto = async () => {
 			setIsloading(true);
-			await fetch("https://api.coincap.io/v2/assets").then((response) =>
-			response.json().then((apiData) => {
-				apiData.data.forEach(coin => {
-console.log(coin.marketCapUsd)
-				let marketCR = Math.round(
-						(Number(coin.marketCapUsd) + Number.EPSILON) * 100
-					) / 100
 
-					const newCoin = {
-						rank: coin.rank,
-						name: coin.symbol + ' ' + coin.name,
-						price:
-							Math.round(
-								(Number(coin.priceUsd) + Number.EPSILON) * 100
-							) / 100,
-						change:
-							Math.round(
-								(Number(coin.changePercent24Hr) + Number.EPSILON) * 100
-							) /
-								100 +
-							"%",
-						marketCap: roundMarketCap(marketCR),					
-						volume:
-							Math.round(
-								(Number(coin.vwap24Hr) + Number.EPSILON) * 100
-							) / 100
-					}
-					
-					setData((d) => ([
-						...d,
-						newCoin
-							]));
-				});
-			})
-		);
-		setIsloading(false);
+	try {
+			await fetch("https://api.coincap.io/v2/assets").then((response) =>
+		response.json().then((apiData) => {
+			apiData.data.forEach(coin => {
+console.log(coin.marketCapUsd)
+			let marketCR = Math.round(
+					(Number(coin.marketCapUsd) + Number.EPSILON) * 100
+				) / 100
+
+				const newCoin = {
+					rank: coin.rank,
+					name: coin.symbol + ' ' + coin.name,
+					price:
+						Math.round(
+							(Number(coin.priceUsd) + Number.EPSILON) * 100
+						) / 100,
+					change:
+						Math.round(
+							(Number(coin.changePercent24Hr) + Number.EPSILON) * 100
+						) /
+							100 +
+						"%",
+					marketCap: roundMarketCap(marketCR),					
+					volume:
+						Math.round(
+							(Number(coin.vwap24Hr) + Number.EPSILON) * 100
+						) / 100
+				}
+				
+				setData((d) => ([
+					...d,
+					newCoin
+						]));
+			});
+		})
+	);
+} catch(e){
+		setError(e);
+		}	finally {
+			setIsloading(false);
+		}	
 	}
 		fetchCrupto();
 	},[]);
@@ -72,6 +79,10 @@ const roundMarketCap = function(item){
 	if(isLoading) {
 		return <div><Prelaoder /></div>
 }
+
+	if(error) {
+		return <div className="errorDataMessage">Something went wrong! Please try again.</div>
+	}
 
 	return (
 	<ul>
