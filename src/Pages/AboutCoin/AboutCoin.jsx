afd6eeff72a328c9fb5c2cc7012370496af0f7a4
DJ5007 <kupchak78@gmail.com>
2024-04-06 21:24:26 +0200

    Add Header to AboutCoin Page

--- src/Pages/AboutCoin/AboutCoin.jsx
+++ src/Pages/AboutCoin/AboutCoin.jsx
@@ -1,4 +1,5 @@
 import { useLocation, useParams } from "react-router-dom";
+import './AboutCoin.css'
 
 const AboutCoin = () => {
 	const { id } = useParams();
@@ -14,17 +15,35 @@ const AboutCoin = () => {
 
 	let filteredCoin = filteredDataFunction()[0];
 
+	const date = new Date().toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"})
+
+	const changeColor24 = function (){
+		let color
+		let changeFirstChar = Array.from(`${filteredCoin.change}`)[0];
+		if (changeFirstChar === '-') {
+			color = 'hsl(351.16deg 84.07% 44.31%)'
+		} else {
+			color = 'hsl(114.09deg 100% 54.74%)'
+		}
+		return color
+	}
+	console.log(changeColor24())
+
+	const chang24Style = {
+		color: `${changeColor24()}`,
+	}
+	
 	return (
 		<>
-			<div className="container">
-				<p className="coin-symbol">
-					<span>{filteredCoin.symbol}</span>
-					{filteredCoin.name}
+			<div className="header-aboutCoin">
+				<p className="coin-name">
+					<span>{filteredCoin.name}</span> ( {filteredCoin.symbol})<br/>
+					<span className="currentDate">{date}</span>
 				</p>
-				<p>{filteredCoin.price}</p>
-				<p>{filteredCoin.change}</p>
-				<p>{filteredCoin.marketCap}</p>
-				<p>{filteredCoin.volume}</p>
+				<p>Price<br/>{filteredCoin.price}</p>
+				<p>Change(24H)<br/><span style={chang24Style}>{filteredCoin.change}</span></p>
+				<p>Market Cap<br/>{filteredCoin.marketCap}</p>
+				<p>Volume<br/>{filteredCoin.volume}</p>
 			</div>
 		</>
 	);
